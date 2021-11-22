99b47ccc4421c1c75cfeba4c7ceb6fd563294058
nathan-zaloum <51972215+nathan-zaloum@users.noreply.github.com>
2021-11-22 16:27:11 +1300

    Added styling for block layouts and planned some of the utility functions

--- client/utils.js
+++ client/utils.js
@@ -1,14 +1,42 @@
-// Convert an object of keys into an array
-export const dataToArray = (data) => { return Object.keys(data) }
+export const consonantsArr = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+export const vowelsArr = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
 
-// Format addresses to names (ability-scores => Ability Scores)
-export const formatLink = (text) => {
-    const spaced = text.replace("-", " ")
-    const words = spaced.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
-    return words
+export const checkInput = (char, blockArr) => {
+  switch (blockArr.length) {
+    case 0:
+      if (checkConsonant(char)) {
+        blockArr.push(char)
+      }
+      return blockArr
+  }
+  if (blockArr.length === 0) {
+    if (checkConsonant(char)) {
+      blockArr.push(char)
+    }
+    return blockArr
+  }
 }
 
-// Get the last paramater of a URL
-export const getLastParam = (url, count) => {
-    return url.split('/').slice(-count).join('/')
+const checkConsonant = (char) => {
+  if (consonantsArr.includes(char)) {
+    return true
+  } else {
+    return false
+  }
 }
+
+// const [block, setBlock] = useState([])
+
+// VALID CHARACTER CHECKS
+
+// check if the first is consonant
+// check if the second is vowel
+// check if there is at most 2 vowels in a row
+// check if there is at most 2 consonants in a row after vowels
+// check if there is at most 5 characters in the block
+
+// BLOCK STRUCTURE RULES
+
+// check if the first vowel is short or tall
+// check if the block contains a double vowel
+// check if the block contains a double consonant after vowels
